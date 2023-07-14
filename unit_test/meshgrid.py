e13eb2bed632db5a48e36deca1ea1dfe3009f3e2
Herrington Jiao <mr.fool@foxmail.com>
2023-07-14 14:06:01 +0800

    Clean workspace

--- unit_test/meshgrid.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import tensorflow as tf
-
-h, w = 240, 320
-
-spatial_feat_np = np.mgrid[0:h, 0:w][::-1].transpose((1, 2, 0))
-# ys_tf, xs_tf = tf.meshgrid(tf.range(h), tf.range(w), indexing="ij")
-
-print(spatial_feat_np.shape)
